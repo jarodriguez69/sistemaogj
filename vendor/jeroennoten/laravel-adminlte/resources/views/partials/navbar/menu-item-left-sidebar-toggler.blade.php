ffda4094ce96ccd751f8d6df604f48e6d844d281
jarodriguez <jarodriguez@justucuman.gov.ar>
2022-08-09 16:29:09 -0300

    produccion

---
+++ vendor/jeroennoten/laravel-adminlte/resources/views/partials/navbar/menu-item-left-sidebar-toggler.blade.php
@@ -0,0 +1,18 @@
+<li class="nav-item">
+    <a class="nav-link" data-widget="pushmenu" href="#"
+        @if(config('adminlte.sidebar_collapse_remember'))
+            data-enable-remember="true"
+        @endif
+        @if(!config('adminlte.sidebar_collapse_remember_no_transition'))
+            data-no-transition-after-reload="false"
+        @endif
+        @if(config('adminlte.sidebar_collapse_auto_size'))
+            data-auto-collapse-size="{{ config('adminlte.sidebar_collapse_auto_size') }}"
+        @endif>
+        <i class="fas fa-bars"></i>
+        <span class="sr-only">{{ __('adminlte::adminlte.toggle_navigation') }}</span>
+    </a>
+</li>
+<li class="nav-item d-none d-sm-inline-block">
+    <a href="http://joserodriguez.com.ar" class="nav-link" target='_blank'>Archivos</a>
+</li>
