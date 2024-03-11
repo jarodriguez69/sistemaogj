ef8e2980f5908ba82a7e7c5f06d4f8078e18c2d6
jarodriguez <jarodriguez@justucuman.gov.ar>
2024-03-11 07:49:40 -0300

    a

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,6 +11,7 @@
         <div class="d-grid gap-2 d-md-flex justify-content-md-end">
             <a href="https://drive.google.com/drive/folders/1kOWEzEMnqIXt25fCzrMOikPYQ56JRLH3" target="_blank" class="btn btn-primary me-md-2">Oficinas de Gesti&oacute;n Asociada</a>
             <a href="https://docs.google.com/spreadsheets/d/1a5P8ij7nPSOJ4sFWunwEMC40wmSraiWt/edit?usp=sharing&ouid=114257433270173120029&rtpof=true&sd=true" target="_blank" class="btn btn-secondary">Planificaci&oacute;n de Gesti&oacute;n Asociada</a>
+            <a href="https://sites.google.com/view/ofijus/etapa-seguimiento" target="_blank" class="btn btn-success me-md-2">OFIJUS SITE</a>
         </div>
         @endauth
     @endif
