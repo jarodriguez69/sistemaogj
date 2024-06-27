4e1f443da86f1605396a047e5b67e23bd673764b
jarodriguez <jarodriguez@justucuman.gov.ar>
2024-06-27 09:56:33 -0300

    a

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
     <div class="card">
         <div class="card-body" style="text-align: justify;">
             <h2><strong>POLÍTICA DE CALIDAD</strong></h2>
-            <p>La Oficina de Gestión Judicial tiene como misión mejorar la gestión de los diversos órganos del Poder Judicial de la Provincia de Tucumán mediante la implementación de políticas judiciales establecidas por la Excma. Corte.</p>
+            <p>La Oficina de Gestión Judicial tiene como misión mejorar la gestión de los diversos órganos del Poder Judicial de la Provincia de Tucumán mediante la implementación de políticas judiciales establecidas por la Excma. Corte y  coordinar el funcionamiento de las Oficinas de Gestión Asociada de todos los fueros y Centros Judiciales.</p>
             <p>La Oficina de Gestión Judicial manifiesta su compromiso con la Calidad mediante la implementación de un Sistema de Gestión de Calidad basado en la Norma Internacional ISO 9001:2015 y el cumplimiento de sus requisitos para lograr la mejora continua del mismo, realizando su seguimiento en forma continua y asegurando la disponibilidad de los recursos necesarios para su funcionamiento.</p>
     
     
@@ -32,14 +32,17 @@
             
             <h2><strong> OBJETIVOS DE CALIDAD</strong><a target="_blank" href="https://docs.google.com/document/d/1brymCVzHZXBDuYOR6p5dPvC2N1k58mmP/edit?usp=sharing&ouid=115910062807870671157&rtpof=true&sd=true" style="font-size: small; font-weight: bold;"> (Minuta de aprobación) </a>  </h2>  
             <ol>
-                <li>Implementar y mantener el sistema de gestión de calidad ISO 9001:2015.</li>
+            <li>Mantener el sistema de gestión de calidad ISO 9001:2015.</li>
                 <li>Incorporar la medición de la satisfacción de los usuarios de los proyectos.</li>
                 <li>Lograr la satisfacción de los usuarios de los proyectos con medición de satisfacción.</li>
                 <li>Lograr la aprobación del SGC de las Unidades Judiciales del PJT que participan de la implementación de la Norma ISO 9001:2015 bajo la coordinación, asistencia técnica y asesoramiento de la OGJ.</li>
-                <li>Implementar planes de mejora en unidades no penales del PJT - *Abarca la implementación de PO, Asistencias y Control de Procesos para la mejora de unidades del PJT*.</li>
+                <li>Implementar planes de mejora en unidades no penales del PJT - Abarca la implementación de PO para la mejora de unidades del PJT.</li>
                 <li>Incorporar el monitoreo y control de los edictos publicados en el Boletín Oficial Judicial conforme a los plazos definidos.</li>
-                <li>Medir el clima laboral dentro de los plazos establecidos (para las unidades que corresponda**)</li>
+                <li>Medir el clima laboral dentro de los plazos establecidos (para las unidades que corresponda**).</li>
                 <li>Planificar y realizar las auditorías internas a las Unidades Judiciales del PJT que cumplan los requisitos dentro de los plazos establecidos.</li>
+                <li>Coordinar el funcionamiento de las Oficinas del PJT dependientes de la OGJ.</li>
+                <li>Lograr el dictado de las providencias conforme a los plazos procesales.</li>
+                <li>Realizar el seguimiento de las unidades certificadas con la Norma ISO 9001:2015 del PJT.</li>
             </ol>
             ** Unidades certificadas que aceptaron la medición de clima laboral.
            
