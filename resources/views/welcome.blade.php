5f90d2a3bff8dcbda5dd2fdb0a4c2759d5ec9b7e
jarodriguez <jarodriguez@justucuman.gov.ar>
2024-10-18 07:38:50 -0300

    a

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
             <a href="https://docs.google.com/spreadsheets/d/1TmD0Z4sbirdefI-7deqOP7jFINfT-P-o89x4a7gVP5E/edit?gid=1677287769#gid=1677287769" target="_blank" class="btn btn-secondary">Planificaci&oacute;n de Gesti&oacute;n Asociada</a>
             <a href="https://sites.google.com/view/ofijus/etapa-seguimiento" target="_blank" class="btn btn-success me-md-2">SITE OGAS</a>
             <a href="https://drive.google.com/drive/folders/1Sa5YG-P4DBxxVxP72bWu0cgq60nxV2W-?usp=sharing" target="_blank" class="btn btn-info me-md-2">SEGUIMIENTO OGAS</a>
-            <a href="https://drive.google.com/drive/folders/1LGeEhvdomiXQyl_rXMQ3kUIHkPoQz8n3?usp=sharing" target="_blank" class="btn btn-warning me-md-2">PERSONAL OGAS</a>
+            <a href="https://drive.google.com/drive/folders/1GxFw5MkKl1FkV3Hlf643oR5p3FqzATvO" target="_blank" class="btn btn-warning me-md-2">PERSONAL OGAS</a>
         </div>
         @endauth
     @endif
